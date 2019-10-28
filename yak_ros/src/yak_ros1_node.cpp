9c86a94fcc929feaa9996d817545308fd9eff272
Matthew Powelson <matthew.powelson@swri.org>
2019-10-28 15:51:35 -0500

    Refactor ROS 1 server to have a header file
    It was becoming a bit unwieldy.

--- yak_ros/src/yak_ros1_node.cpp
+++ yak_ros/src/yak_ros1_node.cpp
@@ -16,177 +16,128 @@
 #include <yak_ros_msgs/UpdateKinFuParams.h>
 #include <yak_ros/utils.h>
 
+#include <yak_ros/online_fusion_server_ros1.h>
+
+using namespace yak_ros;
+
 static const std::double_t DEFAULT_MINIMUM_TRANSLATION = 0.00001;
 
-/**
- * @brief The OnlineFusionServer class. Integrate depth images into a TSDF volume. When requested, mesh the volume using
- * marching cubes. Note that this will work using both simulated and real robots and depth cameras.
- */
-class OnlineFusionServer
+OnlineFusionServer::OnlineFusionServer(ros::NodeHandle& nh,
+                                       const kfusion::KinFuParams& params,
+                                       const std::string& tsdf_frame)
+  : fusion_(params, Eigen::Affine3f::Identity())
+  , params_(params)
+  , robot_tform_listener_(tf_buffer_)
+  , tsdf_frame_to_camera_prev_(Eigen::Affine3d::Identity())
+  , tsdf_frame_(tsdf_frame)
 {
-public:
-  /**
-   * @brief OnlineFusionServer constructor
-   * @param nh - ROS node handle
-   * @param params - KinFu parameters such as TSDF volume size, resolution, etc.
-   * @param tsdf_frame - Transform from world frame to volume origin frame.
-   */
-  OnlineFusionServer(ros::NodeHandle& nh, const kfusion::KinFuParams& params, const std::string& tsdf_frame)
-    : fusion_(params, Eigen::Affine3f::Identity())
-    , params_(params)
-    , robot_tform_listener_(tf_buffer_)
-    , tsdf_frame_to_camera_prev_(Eigen::Affine3d::Identity())
-    , tsdf_frame_(tsdf_frame)
+  // Subscribe to depth images published on the topic named by the depth_topic param. Set up callback to integrate
+  // images when received.
+  depth_image_raw_sub_ = nh.subscribe("input_depth_image", 1, &OnlineFusionServer::onReceivedDepthImg, this);
+
+  // Advertise service for marching cubes meshing
+  generate_mesh_service_ = nh.advertiseService("generate_mesh", &OnlineFusionServer::onGenerateMesh, this);
+
+  // Advertise service to reset tsdf volume
+  reset_tsdf_service_ = nh.advertiseService("reset_tsdf", &OnlineFusionServer::onReset, this);
+
+  // Advertise service to update the params
+  reset_tsdf_service_ = nh.advertiseService("update_params", &OnlineFusionServer::onUpdateParams, this);
+}
+
+void OnlineFusionServer::onReceivedDepthImg(const sensor_msgs::ImageConstPtr& image_in)
+{
+  // Get the camera pose in the world frame at the time when the depth image was generated.
+  ROS_INFO_STREAM("Got depth image");
+  geometry_msgs::TransformStamped transform_tsdf_frame_to_camera;
+  try
   {
-    // Subscribe to depth images published on the topic named by the depth_topic param. Set up callback to integrate
-    // images when received.
-    depth_image_raw_sub_ = nh.subscribe("input_depth_image", 1, &OnlineFusionServer::onReceivedDepthImg, this);
-
-    // Advertise service for marching cubes meshing
-    generate_mesh_service_ = nh.advertiseService("generate_mesh", &OnlineFusionServer::onGenerateMesh, this);
-
-    // Advertise service to reset tsdf volume
-    reset_tsdf_service_ = nh.advertiseService("reset_tsdf", &OnlineFusionServer::onReset, this);
-
-    // Advertise service to update the params
-    reset_tsdf_service_ = nh.advertiseService("update_params", &OnlineFusionServer::onUpdateParams, this);
+    transform_tsdf_frame_to_camera =
+        tf_buffer_.lookupTransform(tsdf_frame_, image_in->header.frame_id, image_in->header.stamp);
   }
-
-private:
-  /**
-   * @brief onReceivedDepthImg - callback for integrating new depth images into the TSDF volume
-   * @param image_in - pointer to new depth image
-   */
-  void onReceivedDepthImg(const sensor_msgs::ImageConstPtr& image_in)
+  catch (tf2::TransformException& ex)
   {
-    // Get the camera pose in the world frame at the time when the depth image was generated.
-    ROS_INFO_STREAM("Got depth image");
-    geometry_msgs::TransformStamped transform_tsdf_frame_to_camera;
-    try
-    {
-      transform_tsdf_frame_to_camera =
-          tf_buffer_.lookupTransform(tsdf_frame_, image_in->header.frame_id, image_in->header.stamp);
-    }
-    catch (tf2::TransformException& ex)
-    {
-      // Abort integration if tf lookup failed
-      ROS_WARN("%s", ex.what());
-      return;
-    }
-    Eigen::Affine3d tsdf_frame_to_camera = tf2::transformToEigen(transform_tsdf_frame_to_camera);
+    // Abort integration if tf lookup failed
+    ROS_WARN("%s", ex.what());
+    return;
+  }
+  Eigen::Affine3d tsdf_frame_to_camera = tf2::transformToEigen(transform_tsdf_frame_to_camera);
 
-    // Find how much the camera moved since the last depth image. If the magnitude of motion was below some threshold,
-    // abort integration. This is to prevent noise from accumulating in the isosurface due to numerous observations from
-    // the same pose.
-    std::double_t motion_mag = (tsdf_frame_to_camera.inverse() * tsdf_frame_to_camera_prev_).translation().norm();
-    ROS_INFO_STREAM(motion_mag);
-    if (motion_mag < DEFAULT_MINIMUM_TRANSLATION)
-    {
-      ROS_INFO_STREAM("Camera motion below threshold");
-      return;
-    }
-
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_in, sensor_msgs::image_encodings::TYPE_16UC1);
-
-    // Integrate the depth image into the TSDF volume
-    if (!fusion_.fuse(cv_ptr->image, tsdf_frame_to_camera.cast<float>()))
-      ROS_WARN_STREAM("Failed to fuse image");
-
-    // If integration was successful, update the previous camera pose with the new camera pose
-    tsdf_frame_to_camera_prev_ = tsdf_frame_to_camera;
+  // Find how much the camera moved since the last depth image. If the magnitude of motion was below some threshold,
+  // abort integration. This is to prevent noise from accumulating in the isosurface due to numerous observations from
+  // the same pose.
+  std::double_t motion_mag = (tsdf_frame_to_camera.inverse() * tsdf_frame_to_camera_prev_).translation().norm();
+  ROS_INFO_STREAM(motion_mag);
+  if (motion_mag < DEFAULT_MINIMUM_TRANSLATION)
+  {
+    ROS_INFO_STREAM("Camera motion below threshold");
     return;
   }
 
-  /**
-   * @brief onGenerateMesh - Perform marching cubes meshing on the TSDF volume and save the result as a binary .ply
-   * file.
-   * @param req
-   * @param res
-   * @return
-   */
-  bool onGenerateMesh(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res)
+  cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_in, sensor_msgs::image_encodings::TYPE_16UC1);
+
+  // Integrate the depth image into the TSDF volume
+  if (!fusion_.fuse(cv_ptr->image, tsdf_frame_to_camera.cast<float>()))
+    ROS_WARN_STREAM("Failed to fuse image");
+
+  // If integration was successful, update the previous camera pose with the new camera pose
+  tsdf_frame_to_camera_prev_ = tsdf_frame_to_camera;
+  return;
+}
+
+bool OnlineFusionServer::onGenerateMesh(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res)
+{
+  (void)req;
+  yak::MarchingCubesParameters mc_params;
+  mc_params.scale = static_cast<double>(params_.volume_resolution);
+  pcl::PolygonMesh mesh = yak::marchingCubesCPU(fusion_.downloadTSDF(), mc_params);
+  ROS_INFO_STREAM("Meshing done, saving ply");
+  pcl::io::savePLYFileBinary("cubes.ply", mesh);
+  ROS_INFO_STREAM("Saving done");
+  res.success = true;
+  return true;
+}
+
+bool OnlineFusionServer::onReset(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res)
+{
+  (void)req;
+  if (fusion_.reset())
   {
-    yak::MarchingCubesParameters mc_params;
-    mc_params.scale = static_cast<double>(params_.volume_resolution);
-    pcl::PolygonMesh mesh = yak::marchingCubesCPU(fusion_.downloadTSDF(), mc_params);
-    ROS_INFO_STREAM("Meshing done, saving ply");
-    pcl::io::savePLYFileBinary("cubes.ply", mesh);
-    ROS_INFO_STREAM("Saving done");
+    ROS_INFO_STREAM("TSDF volume has been reset");
     res.success = true;
     return true;
   }
-
-  /**
-   * @brief Resets the tsdf volume
-   * @param req Service request argument
-   * @param res Service response argument
-   * @return Returns true if successful
-   */
-  bool onReset(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res)
+  else
   {
-    if (fusion_.reset())
-    {
-      ROS_INFO_STREAM("TSDF volume has been reset");
-      res.success = true;
-      return true;
-    }
-    else
-    {
-      ROS_ERROR_STREAM("TSDF volume has not been reset");
-      res.success = false;
-      return false;
-    }
+    ROS_ERROR_STREAM("TSDF volume has not been reset");
+    res.success = false;
+    return false;
   }
+}
 
-  /**
-   * @brief Updates the KinFu params and also resets the TSDF volume
-   * @param req Service request argument
-   * @param res Service response argument
-   * @return Returns true if successful
-   */
-  bool onUpdateParams(yak_ros_msgs::UpdateKinFuParamsRequest& req, yak_ros_msgs::UpdateKinFuParamsResponse& res)
+bool OnlineFusionServer::onUpdateParams(yak_ros_msgs::UpdateKinFuParamsRequest& req,
+                                        yak_ros_msgs::UpdateKinFuParamsResponse& res)
+{
+  if (!yak_ros::updateParams(params_, req))
   {
-    if (!yak_ros::updateParams(params_, req))
-    {
-      ROS_ERROR_STREAM("Update KinFuParams failed. TSDF volume has not been reset.");
-      res.success = false;
-      return false;
-    }
-    if (!fusion_.resetWithNewParams(params_))
-    {
-      ROS_ERROR_STREAM("Resetting Fusion Server Failed.");
-      res.success = false;
-      return false;
-    }
-    else
-    {
-      ROS_INFO_STREAM("TSDF volume has been reset and " << req.params_to_update.size()
-                                                        << " parameters have been updated.");
-      res.success = true;
-      return true;
-    }
+    ROS_ERROR_STREAM("Update KinFuParams failed. TSDF volume has not been reset.");
+    res.success = false;
+    return false;
   }
-
-  yak::FusionServer fusion_;
-  kfusion::KinFuParams params_;
-
-  /** @brief Subscriber that listens to incoming unrectofied depth images */
-  ros::Subscriber depth_image_raw_sub_;
-  /** @brief Buffer used to store locations of the camera (*/
-  tf2_ros::Buffer tf_buffer_;
-  /** @brief Listener used to look up tranforms for the location of the camera */
-  tf2_ros::TransformListener robot_tform_listener_;
-  /** @brief Service that is called to trigger marching cubes and saving the mesh */
-  ros::ServiceServer generate_mesh_service_;
-  /** @brief Service that resets the tsdf volue */
-  ros::ServiceServer reset_tsdf_service_;
-  /** @brief Service that updates the KinFuParams and resets the TSDF volume */
-  ros::ServiceServer update_params_service_;
-  /** @brief Used to track if the camera has moved. Only add image if it has */
-  Eigen::Affine3d tsdf_frame_to_camera_prev_;
-  /** @brief TF frame associated with the TSDF volume. */
-  std::string tsdf_frame_;
-};
+  if (!fusion_.resetWithNewParams(params_))
+  {
+    ROS_ERROR_STREAM("Resetting Fusion Server Failed.");
+    res.success = false;
+    return false;
+  }
+  else
+  {
+    ROS_INFO_STREAM("TSDF volume has been reset and " << req.params_to_update.size()
+                                                      << " parameters have been updated.");
+    res.success = true;
+    return true;
+  }
+}
 
 /**
  * @brief main - Initialize the tsdf_node
@@ -207,16 +158,16 @@ int main(int argc, char** argv)
   // Get camera intrinsics from params
   XmlRpc::XmlRpcValue camera_matrix;
   pnh.getParam("camera_matrix", camera_matrix);
-  default_params.intr.fx = static_cast<double>(camera_matrix[0]);
-  default_params.intr.fy = static_cast<double>(camera_matrix[4]);
-  default_params.intr.cx = static_cast<double>(camera_matrix[2]);
-  default_params.intr.cy = static_cast<double>(camera_matrix[5]);
+  default_params.intr.fx = static_cast<float>(static_cast<double>(camera_matrix[0]));
+  default_params.intr.fy = static_cast<float>(static_cast<double>(camera_matrix[4]));
+  default_params.intr.cx = static_cast<float>(static_cast<double>(camera_matrix[2]));
+  default_params.intr.cy = static_cast<float>(static_cast<double>(camera_matrix[5]));
 
   ROS_INFO("Camera Intr Params: %f %f %f %f\n",
-           default_params.intr.fx,
-           default_params.intr.fy,
-           default_params.intr.cx,
-           default_params.intr.cy);
+           static_cast<double>(default_params.intr.fx),
+           static_cast<double>(default_params.intr.fy),
+           static_cast<double>(default_params.intr.cx),
+           static_cast<double>(default_params.intr.cy));
 
   // Set up TSDF parameters
   // TODO: Autocompute resolution from volume length/width/height in meters
