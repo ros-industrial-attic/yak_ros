a02f5df55892db9d32a34a4ce4856a549044357e
Matthew Powelson <powelson.matthew@gmail.com>
2020-02-21 07:07:16 -0600

    fixed typo

--- yak_tester/src/yak_tester_node.cpp
+++ yak_tester/src/yak_tester_node.cpp
@@ -1,11 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
-//#include "yak_ros_msgs/srv/generate_mesh.hpp"
 
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  auto node = rclpp::Node::make_shared("yak_tester_node");
+  auto node = rclcpp::Node::make_shared("yak_tester_node");
   //node->create_client<yak_ros_msgs::srv::GenerateMesh>("generate_mesh_service");
   rclcpp::shutdown();
   return 0;
