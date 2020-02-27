c14ffdba60aaa5beb8b676604abec7631898f578
Joseph Schornak <joe.schornak@gmail.com>
2020-02-26 18:57:43 -0600

    Update ROS2 yak_ros node

--- yak_ros/src/yak_ros2_node.cpp
+++ yak_ros/src/yak_ros2_node.cpp
@@ -15,8 +15,8 @@
 
 #include <pcl/io/ply_io.h>
 
-static const std::string DEFAULT_DEPTH_IMAGE_TOPIC = "/camera/depth/image_rect_raw";
-static const std::double_t DEFAULT_MINIMUM_TRANSLATION = 0.00001;
+static const std::string DEFAULT_DEPTH_IMAGE_TOPIC = "input_depth_image";
+//static const std::double_t DEFAULT_MINIMUM_TRANSLATION = 0.00001;
 
 /**
  * @brief The Fusion class. Integrate depth images into a TSDF volume. When requested, mesh the volume using marching cubes.
@@ -25,6 +25,7 @@ static const std::double_t DEFAULT_MINIMUM_TRANSLATION = 0.00001;
 class Fusion
 {
 public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   /**
    * @brief OnlineFusionServer constructor
@@ -33,11 +34,11 @@ public:
    * @param world_to_volume - Transform from world frame to volume origin frame.
    * @param tsdf_base_frame - The name of the tf frame that will be used as the base frame when performing lookups between the TSDF volume and the camera frame.
    */
-  explicit Fusion(const rclcpp::Node::SharedPtr node, const kfusion::KinFuParams& params, const Eigen::Affine3f& world_to_volume, const std::string tsdf_base_frame) :
+  explicit Fusion(const rclcpp::Node::SharedPtr node, const kfusion::KinFuParams& params, const std::string tsdf_base_frame) :
     node_(node),
-    fusion_(params, world_to_volume),
+    fusion_(params, Eigen::Affine3f::Identity()),
     params_(params),
-    clock_(std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME)),
+    clock_(std::make_shared<rclcpp::Clock>(RCL_ROS_TIME)),
     tf_buffer_(clock_),
     robot_tform_listener_(tf_buffer_),
     world_to_camera_prev_(Eigen::Affine3d::Identity()),
@@ -54,25 +55,14 @@ public:
         try
         {
           transform_world_to_camera = tf_buffer_.lookupTransform(tsdf_base_frame_, image_in->header.frame_id, tf2::TimePoint(std::chrono::seconds(image_in->header.stamp.sec) + std::chrono::nanoseconds(image_in->header.stamp.nanosec)), tf2::Duration(1000000000));
-         // transform_world_to_camera = tf_buffer_.lookupTransform("table", "camera_base_link", tf2::TimePoint(std::chrono::seconds(image_in->header.stamp.sec) + std::chrono::nanoseconds(image_in->header.stamp.nanosec)));
         }
         catch(tf2::TransformException &ex)
         {
           // Abort integration if tf lookup failed
-         RCLCPP_WARN(node_->get_logger(), "%s", ex.what());
+          RCLCPP_WARN(node_->get_logger(), "%s", ex.what());
           return;
         }
         Eigen::Affine3d world_to_camera = tf2::transformToEigen(transform_world_to_camera);
-
-        // Find how much the camera moved since the last depth image. If the magnitude of motion was below some threshold, abort integration.
-        // This is to prevent noise from accumulating in the isosurface due to numerous observations from the same pose.
-        std::double_t motion_mag = (world_to_camera.inverse() * world_to_camera_prev_).translation().norm();
-
-        if(motion_mag < DEFAULT_MINIMUM_TRANSLATION)
-        {
-          RCLCPP_INFO(node_->get_logger(), "Camera motion below threshold");
-          return;
-        }
 
         cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_in, sensor_msgs::image_encodings::TYPE_16UC1);
 
@@ -104,7 +94,7 @@ public:
     };
 
     // Advertise service for marching cubes meshing
-    generate_mesh_service_ = node_->create_service<std_srvs::srv::Trigger>("generate_mesh_service", generate_mesh_cb);
+    generate_mesh_service_ = node_->create_service<std_srvs::srv::Trigger>("generate_mesh", generate_mesh_cb);
   }
 
 private:
@@ -127,48 +117,76 @@ int main(int argc, char * argv[])
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("fusion_node");
 
-    std::string tsdf_base_frame;
-    node->get_parameter_or<std::string>("tsdf_base_frame", tsdf_base_frame, "turntable_platform");
+
 
     // Set up TSDF parameters
-    kfusion::KinFuParams default_params = kfusion::KinFuParams::default_params();
+    kfusion::KinFuParams kinfu_params = kfusion::KinFuParams::default_params();
 
-    node->get_parameter_or("kinfu_params.use_pose_hints", default_params.use_pose_hints, true);  // use robot forward kinematics to find camera pose relative to TSDF volume
-    node->get_parameter_or("kinfu_params.use_icp", default_params.use_icp, false);  // since we're using robot FK to get the camera pose, don't use ICP (TODO: yet!)
-    node->get_parameter_or("kinfu_params.update_via_sensor_motion", default_params.update_via_sensor_motion, false);  // deprecated?
+    node->declare_parameter("cols", rclcpp::ParameterValue(640));
+    node->declare_parameter("rows", rclcpp::ParameterValue(480));
+    node->declare_parameter("cx", rclcpp::ParameterValue(320.0));
+    node->declare_parameter("cy", rclcpp::ParameterValue(240.0));
+    node->declare_parameter("fx", rclcpp::ParameterValue(550.0));
+    node->declare_parameter("fy", rclcpp::ParameterValue(550.0));
+    node->declare_parameter("volume_x", rclcpp::ParameterValue(640));
+    node->declare_parameter("volume_y", rclcpp::ParameterValue(640));
+    node->declare_parameter("volume_z", rclcpp::ParameterValue(640));
+    node->declare_parameter("volume_resolution", rclcpp::ParameterValue(0.002));
+    node->declare_parameter("tsdf_frame", rclcpp::ParameterValue("tsdf_frame"));
+    node->declare_parameter("bilateral_sigma_depth", rclcpp::ParameterValue(0.04f));
+    node->declare_parameter("bilateral_sigma_spatial", rclcpp::ParameterValue(4.5f));
+    node->declare_parameter("bilateral_kernel_size", rclcpp::ParameterValue(7));
+    node->declare_parameter("tsdf_min_camera_movement", rclcpp::ParameterValue(0.f));
+    node->declare_parameter("tsdf_trunc_dist");
+    node->declare_parameter("tsdf_max_weight", rclcpp::ParameterValue(50));
+    node->declare_parameter("raycast_step_factor", rclcpp::ParameterValue(0.25));
+    node->declare_parameter("gradient_delta_factor", rclcpp::ParameterValue(0.25));
+    node->declare_parameter("light_pose_x", rclcpp::ParameterValue(0.f));
+    node->declare_parameter("light_pose_y", rclcpp::ParameterValue(0.f));
+    node->declare_parameter("light_pose_z", rclcpp::ParameterValue(0.f));
+    node->declare_parameter("update_via_sensor_motion", rclcpp::ParameterValue(false));
 
-    node->get_parameter_or("kinfu_params.intr.fx", default_params.intr.fx, 380.4028625488281f);
-    node->get_parameter_or("kinfu_params.intr.fy", default_params.intr.fy, 380.81707763671875f);
-    node->get_parameter_or("kinfu_params.intr.cx", default_params.intr.cx, 316.48406982421875f);
-    node->get_parameter_or("kinfu_params.intr.fy", default_params.intr.cy, 245.3903045654297f);
+    std::string tsdf_base_frame;
+    node->get_parameter("tsdf_frame", tsdf_base_frame);
+
+    node->get_parameter("update_via_sensor_motion", kinfu_params.update_via_sensor_motion);  // deprecated?
+
+    node->get_parameter("fx", kinfu_params.intr.fx);
+    node->get_parameter("fy", kinfu_params.intr.fy);
+    node->get_parameter("cx", kinfu_params.intr.cx);
+    node->get_parameter("fy", kinfu_params.intr.cy);
 
     float world_to_vol_x, world_to_vol_y, world_to_vol_z;
-    node->get_parameter_or("kinfu_params.world_to_volume.x", world_to_vol_x,  -0.5f);
-    node->get_parameter_or("kinfu_params.world_to_volume.y", world_to_vol_y,  -0.5f);
-    node->get_parameter_or("kinfu_params.world_to_volume.z", world_to_vol_z,  -0.25f);
+    node->get_parameter("kinfu_params.world_to_volume.x", world_to_vol_x);
+    node->get_parameter("kinfu_params.world_to_volume.y", world_to_vol_y);
+    node->get_parameter("kinfu_params.world_to_volume.z", world_to_vol_z);
     Eigen::Affine3f world_to_volume (Eigen::Affine3f::Identity());
     world_to_volume.translation() += Eigen::Vector3f(world_to_vol_x, world_to_vol_y, world_to_vol_z);
 
     int voxels_x, voxels_y, voxels_z;
-    node->get_parameter_or("kinfu_params.volume_dims_voxels.x", voxels_x,  320);
-    node->get_parameter_or("kinfu_params.volume_dims_voxels.y", voxels_y,  320);
-    node->get_parameter_or("kinfu_params.volume_dims_voxels.z", voxels_z,  320);
+    node->get_parameter("volume_x", voxels_x);
+    node->get_parameter("volume_y", voxels_y);
+    node->get_parameter("volume_z", voxels_z);
 
-    double voxel_resolution;
-    node->get_parameter_or("kinfu_params.voxel_resolution_meters", voxel_resolution,  0.003);
+    float volume_resolution;
+    node->get_parameter("volume_resolution", volume_resolution);
 
     // TODO: Autocompute resolution from volume length/width/height in meters
-    default_params.volume_dims = cv::Vec3i(voxels_x, voxels_y, voxels_z);
-    default_params.volume_resolution = voxel_resolution;
-    default_params.volume_pose = Eigen::Affine3f::Identity(); // This gets overwritten when Yak is initialized
-    default_params.tsdf_trunc_dist = default_params.volume_resolution * 5.0f; //meters;
-    default_params.tsdf_max_weight = 50;   //frames
-    default_params.raycast_step_factor = 0.25;  //in voxel sizes
-    default_params.gradient_delta_factor = 0.25; //in voxel sizes
+    kinfu_params.volume_dims = cv::Vec3i(voxels_x, voxels_y, voxels_z);
+    kinfu_params.volume_resolution = volume_resolution;
+    kinfu_params.volume_pose = Eigen::Affine3f::Identity(); // This gets overwritten when Yak is initialized
+    kinfu_params.use_pose_hints = true;  // use robot forward kinematics to find camera pose relative to TSDF volume
+    kinfu_params.use_icp = false;        // since we're using robot FK to get the camera pose, don't use ICP (TODO: yet!)
+
+    double trunc_dist;
+    node->get_parameter_or("tsdf_trunc_dist", kinfu_params.tsdf_trunc_dist, 5.0f * volume_resolution); //meters;
+    node->get_parameter("tsdf_max_weight", kinfu_params.tsdf_max_weight);
+    node->get_parameter("raycast_step_factor", kinfu_params.raycast_step_factor);
+    node->get_parameter("gradient_delta_factor", kinfu_params.gradient_delta_factor);
 
     RCLCPP_INFO(node->get_logger(), "Starting fusion node");
 
-    auto fusion = std::make_shared<Fusion>(node, default_params, world_to_volume, tsdf_base_frame);
+    auto fusion = std::make_shared<Fusion>(node, kinfu_params, tsdf_base_frame);
 
     rclcpp::spin(node);
     rclcpp::shutdown();
